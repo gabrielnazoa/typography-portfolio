1069e114ba7c0b534b0fa664df9872225810d1f2
Gabriel Nazoa <hello@gabrielnazoa.com>
2022-02-11 18:17:40 +0100

    second commit

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,21 +1,33 @@
-/* Dropdown Menu */
-  function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-    document.getElementById("myDropdown").classList.toggle("headerFullHeight");
-  }
-  
-  window.onclick = function(event) {
-    if (!event.target.matches('.droplink')) {
-      let dropdowns = document.getElementsByClassName("dropdown-content");
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-        if (openDropdown.classList.contains('headerFullHeight')) {
-            openDropdown.classList.remove('headerFullHeight');
-          }
-      }
-    }
-  }
+/*Dropdown Menu*/
+
+let menuLink = document.getElementById('menu-link');
+let menuLink2 = document.getElementById('menu-link-2');
+let menuContent = document.getElementById('menu-content');
+let header = document.querySelector('header')
+let bottomArea = document.getElementById('menu-bottom-area');
+let closeButton = document.getElementById('close-button');
+
+let open = function() {
+  menuContent.style.display = 'block';
+  header.style.height = '100vh';
+  header.style.display = 'grid';
+  bottomArea.style.display = 'block';
+  closeButton.style.display = 'block';
+  menuLink.style.display = 'none';
+  menuLink2.style.display = 'block';
+  document.body.style.overflow = 'hidden'
+};
+
+let close = function() {
+  menuContent.style.display = 'none';
+  header.style.height = 'auto';
+  header.style.display = 'block';
+  bottomArea.style.display = 'none';
+  closeButton.style.display = 'none';
+  menuLink.style.display = 'block';
+  menuLink2.style.display = 'none';
+  document.body.style.overflow = 'auto'
+}
+
+menuLink.addEventListener('click', open);
+closeButton.addEventListener('click', close);
